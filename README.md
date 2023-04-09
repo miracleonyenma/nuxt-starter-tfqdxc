99f20a6828018710a0bc2e737cdb896d8db7f3e8
miracleonyenma <42788603+miracleonyenma@users.noreply.github.com>
2023-04-09 01:19:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-tfqdxc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-tfqdxc)
---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <PhHorse />
+    <PhHeart :size="32" color="hotpink" weight="fill" />
+    <PhCube />
+  </div>
+</template>
+<script lang="ts" setup>
+import { PhHorse, PhHeart, PhCube } from '@phosphor-icons/vue';
+</script>
